c9e463646d29076e71d95c17a2eb6066511f02d6
pontusnordstrom89 <pontusnordstrom89@users.noreply.github.com>
2020-11-21 15:43:37 +0100

    Fet fix
    Fixat det mesta

---
+++ ronaparty-frontend/src/RonaParty.js
@@ -0,0 +1,29 @@
+
+import './RonaParty.css';
+
+
+function RonaParty() {
+  return (
+    <div className="wrapper">
+      <header className="RonaParty-header">
+        <div className="header-text">
+          <h1>RonaParty</h1>
+        </div>
+
+        <div className="admin-tools">
+          <button id="admin-tools" type="button">Admin-Tools</button>
+        </div>
+
+      </header>
+
+
+      <main className="GameFrame">
+        hej
+      </main>
+
+
+    </div>
+  );
+}
+
+export default RonaParty;
--- ronaparty-frontend/src/index.js
+++ ronaparty-frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RonaParty from './RonaParty.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RonaParty />
   </React.StrictMode>,
   document.getElementById('root')
 );
