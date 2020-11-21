13f599de38b66c06c1d10679f2bf341d952b0925
pontusnordstrom89 <pontusnordstrom89@users.noreply.github.com>
2020-11-22 00:04:47 +0100

    Simon suger
    Simon suger

--- ronaparty-frontend/src/pages/fragments/scoreboard/index.js
+++ ronaparty-frontend/src/pages/fragments/scoreboard/index.js
@@ -2,45 +2,41 @@ import './scoreboard.css';
 
 function Scoreboard() {
   return (
-    <div className="scoreboard">
-    <ul>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-    </ul>
+    <div id="scoreboardWrapper">
+      <h2 id="scoreboardHeader">Scoreboard</h2>
+
+      <div id="scores">
+        <ol>
+          <li>Ronny</li>
+          <li>Sonny</li>
+          <li>Jimmy</li>
+          <li>Jonny</li>
+          <li>Lenny</li>
+          <li>Kenny</li>
+          <li>Benny</li>
+          <li>Ronny</li>
+          <li>Sonny</li>
+          <li>Jimmy</li>
+          <li>Jonny</li>
+          <li>Lenny</li>
+          <li>Kenny</li>
+          <li>Benny</li>
+          <li>Ronny</li>
+          <li>Sonny</li>
+          <li>Jimmy</li>
+          <li>Jonny</li>
+          <li>Lenny</li>
+          <li>Kenny</li>
+          <li>Benny</li>
+          <li>Ronny</li>
+          <li>Sonny</li>
+          <li>Jimmy</li>
+          <li>Jonny</li>
+          <li>Lenny</li>
+          <li>Kenny</li>
+          <li>Benny</li>
+        </ol>
+      </div>
 
     </div>
   );
